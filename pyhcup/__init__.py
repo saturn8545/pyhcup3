b1b06ba8cf311e0571f299fd7f66678e102fd13a
saturn8545 <christine.marston@gmail.com>
2024-03-22 16:03:50 -0600

    Update __init__.py
    update import statements for python 3

--- pyhcup/__init__.py
+++ pyhcup/__init__.py
@@ -1,6 +1,13 @@
 #specify modules here mostly for the benefit of dev environments
 #otherwise some will offer crazy stuff for tab-completion and the like
-import db, hachoir, mapper, meta, parser, pd, sas, tx
+from .db import *
+from .hachoir import *
+from .mapper import *
+from .meta import *
+from .parser import *
+from .pd import *
+from .sas import *
+from .tx import *
 from .parser import read, replace_sentinels, default_uflaggers, UtilizationFlagger
 from .sas import meta_from_sas
 from .meta import get as get_meta
